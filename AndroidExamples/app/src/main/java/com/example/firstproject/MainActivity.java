6c44d6e0a5f954681dcc40ad3c74add0d4153b33
Praveen1756 <pmashetty@jazeeratechnologies.com>
2019-07-25 08:27:57 +0530

    Login Screen

--- AndroidExamples/app/src/main/java/com/example/firstproject/MainActivity.java
+++ AndroidExamples/app/src/main/java/com/example/firstproject/MainActivity.java
@@ -4,21 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private String TAG = "MainActivity";
+
+    // Employee employee = new Employee();
+
+    EditText et_username;
+    EditText et_password;
+    Button bt_login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_username = findViewById(R.id.et_username);
+        et_password = findViewById(R.id.et_password);
+        bt_login = findViewById(R.id.bt_login);
+
+        bt_login.setOnClickListener(this);
+
         Log.v(TAG,"onCreate");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+
         Log.v(TAG,"onStart");
     }
 
@@ -50,5 +70,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onRestart() {
         super.onRestart();
         Log.v(TAG,"onRestart");
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if(et_username.getText().toString().length()>=5 && et_password.getText().toString().length()>=5){
+            showToast("Valid Details");
+        } else {
+            showToast("In Valid Details");
+        }
+
+    }
+
+    private void showToast(String message){
+        Toast.makeText(this,message,Toast.LENGTH_LONG).show();
     }
 }
